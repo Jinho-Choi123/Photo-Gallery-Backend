e37a98b122d1839539567ad8130b8547da7eca0e
Jinho-Choi123 <choijinho817@gmail.com>
2021-11-13 10:47:58 +0000

    refactor:Change auth api

--- auth/urls.py
+++ auth/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path
 
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
-from .views import RegisterView
+from .views import RegisterView, LoginView
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', LoginView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
--- auth/views.py
+++ auth/views.py
@@ -7,6 +7,7 @@ from .serializers import RegisterSerializer
 
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 
 ##Register New Users
 @permission_classes((AllowAny,))
@@ -15,4 +16,21 @@ class RegisterView(APIView):
         register = RegisterSerializer(data=request.data)
         register.is_valid(raise_exception=True)
         register.create(request.data)
+        username = register.username
+
         return Response({"msg": "Account Created"}, status=status.HTTP_201_CREATED)
+
+@permission_classes((AllowAny,))
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = User.objects.get(username=username)
+        if user.check_password(password):
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "username": str(username),
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            })
+        return Response({"error": "Invalid Credentials"}, status=status.HTTP_400_BAD_REQUEST)
